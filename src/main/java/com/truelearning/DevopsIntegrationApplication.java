28484a7e97f8d0771b7422aa66c3a4274156a2c7
Niladri <niladrimondal.mondal@gmail.com>
2024-09-21 01:25:47 +0530

    updated the K8s app

--- src/main/java/com/truelearning/DevopsIntegrationApplication.java
+++ src/main/java/com/truelearning/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Kubernetes, Niladri's Class";
+		return "Welcome to Kubernetes, We are going to leran k8s";
 	}
 
 	public static void main(String[] args) {
